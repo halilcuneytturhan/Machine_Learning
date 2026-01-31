5e513046134cc6c31cff5aa1a1e60ef215808b00
Halil Cuneyt <halilcavuss21@gmail.com>
2026-01-31 18:11:28 +0300

    Burada da standart scaler ve tablo içerisinde sütun silme işlemleri yapıldı.

--- veri on isleme/veri_onizleme.py
+++ veri on isleme/veri_onizleme.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 #Label encoder verileri 0 ve 1 olarak değiştiriyor.
+from sklearn.preprocessing import StandardScaler
+
 
 # Veriyi yükle
 df = pd.read_excel(r'C:\Users\cavus\Desktop\global\veri on isleme\veri_on_isleme_ve_ozellik_muhendisligi.xlsx')
@@ -11,4 +13,17 @@ df.fillna(df['Gelir'].mean(),inplace= True)
 
 le = LabelEncoder()
 df['Cinsiyet'] = le.fit_transform(df['Cinsiyet'])
+# print(df)
+
+standart = StandardScaler()
+# df[['Yaş','Gelir']] = standart.fit_transform(df[['Yaş','Gelir']])
+# print(df)
+
+#Burada da bins içinde 4 adet yazıyorsak 3 adet etiket yazabiliriz 
+df['Gelir_Grubu'] = pd.cut(df['Gelir'],bins=[0,3000,5000,7000],labels=['Düşük','Orta','Yüksek'])
+# print(df)
+df.drop('ID',axis=1,inplace=True)   
 print(df)
+df.to_excel('Kategorik_Gelir.xlsx',index=False)
+print("Oluşturuldu.")
+
