749f52b8f8c5441a16b1c1871720729903d00911
Halil Cuneyt <halilcavuss21@gmail.com>
2026-01-31 18:33:34 +0300

    2 meslek grubunu kıyaslayacağımız zaman seaborn kütüphanesi kullanılacak.

--- veri on isleme/veri_onizleme.py
+++ veri on isleme/veri_onizleme.py
@@ -2,6 +2,9 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 #Label encoder verileri 0 ve 1 olarak değiştiriyor.
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 
 # Veriyi yükle
@@ -12,7 +15,7 @@ df.fillna(df['Gelir'].mean(),inplace= True)
 # print(df)
 
 le = LabelEncoder()
-df['Cinsiyet'] = le.fit_transform(df['Cinsiyet'])
+# df['Cinsiyet'] = le.fit_transform(df['Cinsiyet'])
 # print(df)
 
 standart = StandardScaler()
@@ -22,8 +25,29 @@ standart = StandardScaler()
 #Burada da bins içinde 4 adet yazıyorsak 3 adet etiket yazabiliriz 
 df['Gelir_Grubu'] = pd.cut(df['Gelir'],bins=[0,3000,5000,7000],labels=['Düşük','Orta','Yüksek'])
 # print(df)
-df.drop('ID',axis=1,inplace=True)   
-print(df)
-df.to_excel('Kategorik_Gelir.xlsx',index=False)
-print("Oluşturuldu.")
+# df.drop('ID',axis=1,inplace=True)   
+# print(df)
+# df.to_excel('Kategorik_Gelir.xlsx',index=False)
+# print("Oluşturuldu.")
+
+
+# Görselleştirme
+# plt.figure(figsize=(10,6))
+# plt.hist(df['Yaş'], # Yaş kısmı ile tablo oluştur.
+#          bins=10, # Bins kısmında da kaç parçaya bölüneceği yazıyor 2 yaparsak 2 grafik, 10 yaparsak 10 grafik
+#          color='skyblue' # Color kısmı barın içerisindeki renk
+#          ,edgecolor='black' # Dışındaki çizgi
+#          )
+# plt.xlabel('Yaş')
+# plt.ylabel('Frekans')
+# plt.title('Yaş Dağılımı')
+
+
+
+# 2 meslek grubunu kıyaslayacağımız zaman seaborn kütüphanesi kullanılacak.
+plt.figure(figsize=(10,6))
+sns.countplot(x='Gelir_Grubu',hue='Yaş',data=df)
+plt.xlabel('Gelir Grubu')
+plt.ylabel('Cinsiyet')
+plt.show()
 
